93bf404919379eff3b7beffdb07111adfc03c9ec
TAKERIE0606\kotak <kotakeshi0923@gmail.com>
2025-06-25 22:48:33 +0900

    "mail function added"

--- src/main/java/com/example/samuraitravel/controller/AuthController.java
+++ src/main/java/com/example/samuraitravel/controller/AuthController.java
@@ -8,12 +8,15 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.entity.VerificationToken;
 import com.example.samuraitravel.event.SignupEventPublisher;
 import com.example.samuraitravel.form.SignupForm;
 import com.example.samuraitravel.service.UserService;
+import com.example.samuraitravel.service.VerificationTokenService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -21,10 +24,13 @@ import jakarta.servlet.http.HttpServletRequest;
 public class AuthController {
     private final UserService userService;
     private final SignupEventPublisher signupEventPublisher;
+    private final VerificationTokenService verificationTokenService;
 
-	public AuthController(UserService userService, SignupEventPublisher signupEventPublisher) {
+    public AuthController(UserService userService, SignupEventPublisher signupEventPublisher, VerificationTokenService verificationTokenService) {
+
         this.userService = userService;
         this.signupEventPublisher = signupEventPublisher;
+        this.verificationTokenService = verificationTokenService;
     }
 
     @GetMapping("/login")
@@ -69,5 +75,21 @@ public class AuthController {
         redirectAttributes.addFlashAttribute("successMessage", "ご入力いただいたメールアドレスに認証メールを送信しました。メールに記載されているリンクをクリックし、会員登録を完了してください。");
 
         return "redirect:/";
+    }
+    @GetMapping("/signup/verify")
+    public String verify(@RequestParam(name = "token") String token, Model model) {
+        VerificationToken verificationToken = verificationTokenService.getVerificationToken(token);
+
+        if (verificationToken != null) {
+            User user = verificationToken.getUser();
+            userService.enableUser(user);
+            String successMessage = "会員登録が完了しました。";
+            model.addAttribute("successMessage", successMessage);
+        } else {
+            String errorMessage = "トークンが無効です。";
+            model.addAttribute("errorMessage", errorMessage);
+        }
+
+        return "auth/verify";
     }
 }
